1a88c2768bd85be461058c10aa97d4847c2ae3c0
Julien Kauffmann <julien.kauffmann@freelan.org>
2015-09-03 21:37:44 -0400

    Added base files

---
+++ doc/source/index.rst
@@ -0,0 +1,22 @@
+.. redis-lua documentation master file, created by
+   sphinx-quickstart on Thu Sep  3 21:32:11 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to redis-lua's documentation!
+=====================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+setup(
+    name='redis-lua',
+    author='Julien Kauffmann',
+    author_email='julien.kauffmann@freelan.org',
+    maintainer='Julien Kauffmann',
+    maintainer_email='julien.kauffmann@freelan.org',
+    version=open('VERSION').read().strip(),
+    description=(
+        "A set of tools to ease LUA scripting when dealing with Redis."
+    ),
+    long_description="""\
+redis-lua provides helpers that deal with redis-py to ease LUA scripting.
+""",
+    packages=find_packages(exclude=[
+        'tests',
+    ]),
+    install_requires=[
+    ],
+    test_suite='tests',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 5 - Production/Stable',
+    ],
+)
